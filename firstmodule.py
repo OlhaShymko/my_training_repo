c96bf17a56d1824b2919878733858e97617430ff
olhash <olha.shymko.91@gmail.com>
2022-08-28 17:51:57 +0300

    Add first changes to commit

---
+++ firstmodule.py
@@ -0,0 +1,19 @@
+# Comment about position the repo
+# Have added comment again
+# New comment in the third branch
+
+
+def start_features(one):
+    return one
+
+
+def medium_feature(new, old):
+    return new / old - 59
+
+
+def next_features(first, third):
+    print("Hello World!" + first + " and " + third)
+
+
+def create_change_for_second_branch():
+    print("I am first function in the second branch")
